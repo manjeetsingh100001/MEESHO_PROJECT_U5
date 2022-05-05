461c874844bcdd0264a9039af44dc04f8452a805
shrawaninilange <shrawaninilange1999@gmail.com>
2022-05-05 21:48:40 +0530

    product pages add

---
+++ app/src/App.js
@@ -0,0 +1,35 @@
+ 
+import './productpages.css';
+import React from 'react';
+import Allproduct from './components/Allproduct';
+ 
+ 
+import Men from './components/men';
+import Beauty from './components/beauty';
+import Footerware from './components/Footerware';
+import Home from './components/Homeproduct';
+import Jewellary from './components/Jewelary';
+import Saree from './components/Saree';
+import Westenware from './components/Westen';
+ 
+ 
+import Kids from './components/KIds';
+import Electronics from './components/electronics';
+
+function App() {                                 
+   
+
+  return (
+     <div id="App">
+        <Allproduct/>
+        
+     </div>
+     
+ 
+
+)
+ 
+ 
+}
+
+export default App;
---
+++ app/src/components/Westen.js
@@ -0,0 +1,72 @@
+import { useEffect ,useState} from  "react"
+import React from "react"
+
+function Westenware() {
+  const [students , setstudent ] = useState([])
+  // const [text , setstudenttext ] = useState("")
+  useEffect(() =>{
+      getdata()
+  })
+
+  const getdata = async () =>{
+      const data = await fetch ("http://localhost:8080/Westenware ").then((d)=>d.json()
+      )
+      setstudent(data)
+  }
+
+  return (
+    <div className="App_pro">
+    <div className="all_products">
+    <h2>Women Ethnic Wear</h2>
+    <p>Showing 1-20 out of 1566598 Products</p>
+    </div>
+ 
+    <div className="pro_con">
+    {
+           students.map((e)=>{
+            return   (
+              (
+     
+       <div className="pro_div">
+      <div className="single_pro">
+        <img src= {e.img} className="pro_img" alt="" />
+        <div className="price">
+        <p>{e.title}</p>
+         <div className="pro_pri">
+         <h2 className="org_pri"> <i class="fa-solid fa-indian-rupee-sign"></i>{e.price} </h2>
+          <h4 className="discount"><i class="fa-solid fa-indian-rupee-sign"></i>{e.d_price}</h4>
+           <p className="dis10">10% discount</p>
+         </div>
+         <p><i class="fa-solid fa-percent"></i> $50 discount on  1st order</p>
+         <div className="freee">
+        <p className="free" >Free delivery</p>
+        </div>
+         <div className="rating_pro">
+           <h2 className="rate">{e.rating}</h2>
+           <h6 className="reviews">39 Reviews</h6>
+         </div>
+        </div>
+      </div>
+       
+    </div>
+ 
+
+)
+) 
+
+
+
+
+})
+}
+         
+
+    </div>
+
+
+       
+     </div>
+  );
+}
+
+export default Westenware;
